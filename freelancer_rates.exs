f8a5f54c706e8c8aae7db36dd770f0ea92a769b0
corsantic <aydofurkan25@gmail.com>
2025-08-17 22:32:55 +0300

    rounding ceiling floor etc

---
+++ freelancer_rates.exs
@@ -0,0 +1,20 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount * (1 - discount / 100.0)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    rate = daily_rate(hourly_rate) * 22
+    ceil(apply_discount(rate, discount))
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    (budget / monthly_rate(hourly_rate, discount) * 22) |> Float.floor(1)
+  end
+end
+
+IO.puts(FreelancerRates.days_in_budget(20000, 80, 11.0))
