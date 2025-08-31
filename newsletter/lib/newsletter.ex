51dfc5478615634f59d49f94ec5514d5ecd9f699
corsantic <aydofurkan25@gmail.com>
2025-08-31 12:31:14 +0300

    switching to project by project approach

---
+++ newsletter/lib/newsletter.ex
@@ -0,0 +1,16 @@
+defmodule Newsletter do
+  def read_emails(path) do
+  end
+
+  def open_log(path) do
+  end
+
+  def log_sent_email(pid, email) do
+  end
+
+  def close_log(pid) do
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+  end
+end
