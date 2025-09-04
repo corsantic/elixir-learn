b7a0fdab900e97e8db43b84a05f1d28beebefdbb
corsantic <aydofurkan25@gmail.com>
2025-09-04 23:51:22 +0300

    erlang libraries

---
+++ captains-log/lib/captains_log.ex
@@ -0,0 +1,22 @@
+defmodule CaptainsLog do
+  @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+
+  def random_planet_class() do
+    Enum.random(@planetary_classes)
+  end
+
+  def random_ship_registry_number() do
+    "NCC-" <> Integer.to_string(Enum.random(1000..9999))
+  end
+
+  def random_stardate() do
+    a = 42000.0
+    b = 41000.0
+    (a - b) * :rand.uniform() + b
+  end
+
+  def format_stardate(stardate) do
+    :io_lib.format("~.1f", [stardate]) |> to_string()
+  end
+end
+
